7142be84dda309c1c17b0d9e010fe3dbb4586eb7
soham <sohamssss456@gmail.com>
2025-06-26 10:46:44 +0530

    db configured with django

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,6 +7,6 @@ from app import calc
 class CalcTests(SimpleTestCase):
 
     def test_add_num(self):
-        res = calc.add(4 , 5)
-        self.assertEqual(res , 9)
+        res = calc.add(4, 5)
+        self.assertEqual(res, 9)
 
